a2c23ff903211a72b6b9f7861db5e303f285c5b9
DanielNaiff <danielnaiff344@gmail.com>
2024-07-21 20:56:45 -0300

    Adicionando meu arquivo inicial

---
+++ src/main/java/me/dio/santander_bootcamp_santander_2024/SantanderBootcampSantander2024Application.java
@@ -0,0 +1,13 @@
+package me.dio.santander_bootcamp_santander_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampSantander2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampSantander2024Application.class, args);
+	}
+
+}
